7639e15a41cdfe0968437e7a18fac47ab640c3ea
Erick Rian <erickrian49@gmail.com>
2025-02-10 21:43:31 -0300

    Iniciado bottom_navigator e AppBar com Drawer.

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,4 +1,7 @@
+import 'package:ecommerce_app/components/bottom_nav_bar.dart';
+import 'shop_page.dart';
 import 'package:flutter/material.dart';
+import 'cart_page.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -8,10 +11,59 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  // Variável de controle para bottom nav bar
+  int _iSelectedIndex = 0;
+
+  // Metodo para atualizar variável de index selecionado
+  void navigateBottomBar(int index) {
+    setState(() {
+      _iSelectedIndex = index;
+    });
+  }
+
+  final List<Widget> _pages = [
+    const ShopPage(),
+
+    const CartPage(),
+  ];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-
+      backgroundColor: Colors.grey[300],
+      bottomNavigationBar: MyBottomNavBar(
+        onTabChange: (index) => navigateBottomBar(index),
+      ),
+      appBar: AppBar(
+        backgroundColor: Colors.transparent,
+        leading: Builder(
+          builder: (context) {
+            return IconButton(
+              icon: Icon(
+                Icons.menu,
+                color: Colors.black,
+              ),
+              onPressed: () {
+                Scaffold.of(context).openDrawer();
+              },
+            );
+          }
+        ),
+      ),
+      drawer: Drawer(
+        backgroundColor: Colors.grey[900],
+        child: Column(
+          children: [
+            DrawerHeader(
+              child: Image.asset(
+                'lib/images/nike_logo.png',
+                color: Colors.white,
+              ),
+            ),
+          ],
+        ),
+      ),
+      body: _pages[_iSelectedIndex],
     );
   }
 }
